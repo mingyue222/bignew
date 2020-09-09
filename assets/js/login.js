fb968bdefd8e6954b0a068385d66c522bf0a1379
ming <2396054307@qq.com>
2020-09-09 17:21:38 +0800

    登陆和注册表单验证完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -6,8 +6,20 @@ $(function() {
         })
         //点击登陆时
     $('#link_login').on('click', function() {
-        $('.reg_box').hide()
-        $('.login_box').show()
+            $('.reg_box').hide()
+            $('.login_box').show()
+        })
+        //自定义验证规则
+        //获取form表单模块
+    var form = layui.form
+    form.verify({
+        password: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        repassword: function(value) {
+            var pwd = $('.reg_box [name=password]').val()
+            if (pwd !== value) {
+                return '两密码输入不一致'
+            }
+        }
     })
 
 })
