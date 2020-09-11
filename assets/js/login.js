056b98ed8347852938b56f0087db47948cdcf79b
ming <2396054307@qq.com>
2020-09-11 11:19:57 +0800

    表单登陆验证注册完成

--- assets/js/login.js
+++ assets/js/login.js
@@ -12,6 +12,7 @@ $(function() {
         //自定义验证规则
         //获取form表单模块
     var form = layui.form
+    var layer = layui.layer
     form.verify({
         password: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
         repassword: function(value) {
@@ -21,5 +22,44 @@ $(function() {
             }
         }
     })
+    $('#form_reg').on('submit', function(e) {
+            e.preventDefault();
+            $.ajax({
+                url: '/api/reguser',
+                type: 'post',
+                data: {
+                    username: $('#form_reg [name=username]').val(),
+                    password: $('#form_reg [name=password]').val()
+                },
+                success: function(res) {
+                    console.log(res);
+                    if (res.status !== 0) {
+                        // return console.log(res.message);
+                        return layer.msg(res.message)
+                    }
+                    // console.log(res.message);
+                    layer.msg(res.message)
+                    $('#link_login').click()
+                }
+            })
+        })
+        //登陆页面
+    $('#form_login').on('submit', function(e) {
+        e.preventDefault()
+        $.ajax({
+            url: '/api/login',
+            type: 'post',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                // console.log(res);
 
+                layer.msg('登陆成功')
+                localStorage.setItem('token', res.token)
+                location.href = '/index.html'
+            }
+        })
+    })
 })
