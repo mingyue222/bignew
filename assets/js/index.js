12fc5e72fae673d0a2fea1d3b84c4f1bce2c92e7
ming <2396054307@qq.com>
2020-09-12 10:08:43 +0800

    主页完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,9 +7,6 @@ $(function() {
             $.ajax({
                 url: '/my/userinfo',
                 type: 'get',
-                headers: {
-                    Authorization: localStorage.getItem('token') || ''
-                },
                 success: function(res) {
                     console.log(res);
                     if (res.status !== 0) {
@@ -18,13 +15,21 @@ $(function() {
                     //更新用户信息成功的话我们要判断用户是否有头象
                     //调用下面的函数
                     randerAvatar(res.data)
-                }
+                },
+                //无论是成功还是失败都要执行complete的回调函数
+                // complete: function(res) {
+                //     console.log(res);
+                //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+                //         localStorage.removeItem('token')
+                //         location.href = 'login.html'
+                //     }
+                // }
             })
         }
 
         function randerAvatar(user) {
             //获取用户的名称
-            var name = user.username || user.user_pic
+            var name = user.username || user.nickname
                 //设置欢迎文本
             $('.welcome').html('欢迎&nbsp;&nbsp' + name)
                 //判断用户有没有设置头像
@@ -39,7 +44,6 @@ $(function() {
         }
     })
     //给退出绑定点击事
-
 $('#btnLogout').on('click', function() {
     layer.confirm('确定退出', { icon: 3, title: '提示' }, function(index) {
         //do something
