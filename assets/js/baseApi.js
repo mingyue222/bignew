f4abc3f4859d354cafea0d0f34332c76b13e68cb
ming <2396054307@qq.com>
2020-09-12 15:11:17 +0800

    基本资料布局完成

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -5,7 +5,7 @@ $.ajaxPrefilter(function(options) {
         options.headers = { Authorization: localStorage.getItem('token') || '' }
     }
     options.complete = function(res) {
-        console.log(res);
+        // console.log(res);
         if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
             localStorage.removeItem('token')
             location.href = 'login.html'
---
+++ assets/js/user/user_info.js
@@ -0,0 +1,48 @@
+$(function() {
+    var form = layui.form
+    var layer = layui.layer
+    form.verify({
+        nickname: function(value) {
+            if (value.length > 6) {
+                return '请输入1-6个的字符'
+            }
+        }
+    })
+    initUserInfo()
+
+    function initUserInfo() {
+        //用户更新请求
+        $.ajax({
+            url: '/my/userinfo',
+            type: 'get',
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                console.log(res);
+                form.val('formUserInfo', res.data)
+
+            }
+        })
+    }
+    //重置表单
+    $('#btnReset').on('click', function(e) {
+            e.preventDefault()
+            initUserInfo()
+        })
+        //更新基本资料
+    $('.layui-form').on('click', function(e) {
+        e.preventDefault()
+        $.ajax({
+            url: '/my/userinfo',
+            type: 'post',
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                // 调用父页面中的方法，重新渲染用户的头像和用户的信息
+                window.parent.initUserInfo()
+            }
+        })
+    })
+})
